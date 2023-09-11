30045f81d76a6c8af533235e071a249160e45520
chrzich2020 <chrzich2020@fau.edu>
2023-09-11 12:06:58 -0400

    Created a view controller, details to the main view, and a custom cell class

---
+++ Flixster/MovieCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieCell.swift
+//  Flixster
+//
+//  Created by Tara Hrzich on 9/11/23.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Flixster/MovieViewController.swift
+++ Flixster/MovieViewController.swift
@@ -2,15 +2,19 @@
 //  ViewController.swift
 //  Flixster
 //
-//  Created by Tara Hrzich on 9/11/23.
+//  Created by Colin Hrzich on 9/11/23.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var movies: [Movie] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        movies = Movie.mockMovie
+        print(movies)
         // Do any additional setup after loading the view.
     }
 
