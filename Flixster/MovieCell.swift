c849f202ce1f6d46d29f84129c7c75d0cf64fd8e
chrzich2020 <chrzich2020@fau.edu>
2023-09-11 14:41:12 -0400

    Added all movie data to Movie.Swift

--- Flixster/MovieCell.swift
+++ Flixster/MovieCell.swift
@@ -2,12 +2,20 @@
 //  MovieCell.swift
 //  Flixster
 //
-//  Created by Tara Hrzich on 9/11/23.
+//  Created by Colin Hrzich on 9/11/23.
 //
 
 import UIKit
+import Nuke
 
 class MovieCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var movieDescLabel: UILabel!
+    
+    
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -20,4 +28,13 @@ class MovieCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+    /// Configures the cell's UI for the given track.
+    func configure(with movie: Movie) {
+        movieTitleLabel.text = movie.movieTitle
+        movieDescLabel.text = movie.movieDesc
+
+        // Load image async via Nuke library image loading helper method
+        Nuke.loadImage(with: movie.artworkUrl100, into: movieImageView)
+    }
+    
 }
--- Flixster/MovieViewController.swift
+++ Flixster/MovieViewController.swift
@@ -7,7 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Get a cell with identifier, "TrackCell"
+        // the `dequeueReusableCell(withIdentifier:)` method just returns a generic UITableViewCell so it's necessary to cast it to our specific custom cell.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+
+        // Get the track that corresponds to the table view row
+        let movie = movies[indexPath.row]
+
+        // Configure the cell with it's associated track
+        cell.configure(with: movie)
+
+        // return the cell for display in the table view
+        return cell
+    }
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
     
     var movies: [Movie] = []
 
@@ -15,7 +37,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         movies = Movie.mockMovie
         print(movies)
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        
     }
 
 
